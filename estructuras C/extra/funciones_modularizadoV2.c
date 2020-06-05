1402cd45462ec8ddf1b121df07e449d2f6011596
Katsuo <49467285+Erick-Katsuo@users.noreply.github.com>
2020-06-05 11:01:32 -0300

    Add files via upload

---
+++ estructuras C/extra/funciones_modularizadoV2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void informar_resultado(float velocidad_obtenida){
+	if( velocidad_obtenida >= 0.0F && velocidad_obtenida <= 90.0F ){
+		printf("Todo ok");
+	}else if( (velocidad_obtenida > 90.0F)  &&  (velocidad_obtenida <= 100.0F) ){
+		printf("Advertencia");
+	}else if( velocidad_obtenida > 100.0F ){
+		printf("Multa");
+	}else{
+		printf("Valor no considerado");
+	}
+}
+
+
+float registrar_velocidad(){
+	float velocidad_registrada;
+	printf("Detectar velocidad\n");
+	scanf("%f", &velocidad_registrada);
+	printf("Velocidad detectada: %f km/h\n", velocidad_registrada);
+	return velocidad_registrada;
+}
+
+
+int main(){
+	informar_resultado( registrar_velocidad() );
+	return 0;
+}
+
