917fda62381af53c352acb57d97369caf71c58fe
Katsuo <49467285+Erick-Katsuo@users.noreply.github.com>
2020-07-16 12:34:28 -0300

    Add files via upload

---
+++ estructuras C/punteros/pasaje_refer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void cambiar_num_contact(int *contacto_actual, int *opcion_recibida){
+	
+	if(opcion_recibida){//true != 0 , false = 0
+		printf("Ingrese nuevo contacto\n");
+		scanf("%i", contacto_actual);// guardarlo en la variable contacto_usuario
+	}
+
+	*opcion_recibida = 35; // cambie el 1 por el 35
+
+	printf("Direccion de memoria que contiene contacto_actual: %p\n", contacto_actual);
+	printf("Direccion de memoria de contacto_actual: %p\n", &contacto_actual);
+	printf("El contenido de la direccion de memoria contenida en contacto_actual %i\n", *contacto_actual);
+
+	printf("opcion_recibida: %i\n", *opcion_recibida);
+}
+
+
+int main(){
+
+	int contacto_usuario = 0;
+	int opcion = 0;
+	printf("Direccion de mem de contacto_usuario en main: %p\n", &contacto_usuario );
+
+	printf("Desea cambiar su numero de contacto?\n Si[1] - No[0]");
+	scanf("%i", &opcion);
+
+	cambiar_num_contact(&contacto_usuario, &opcion);
+
+	printf("contacto usuario %i\n", contacto_usuario );
+	printf("opcion %i\n", opcion);
+
+	return 0;
+}
